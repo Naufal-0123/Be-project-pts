80c20c35fa136c02973a208884b7756437a0b51c
Naufal-0123 <droid0536@gmail.com>
2023-01-20 13:24:47 +0700

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const log = require('./SRC/middleware/log');
 const routing = require('./SRC/Routes/routing');
 const notFound = require('./SRC/middleware/404');
+const {sequelize} = require('./SRC/models')
 const errorHandling = require('./SRC/middleware/error');
 const consoleMid1 = require('./SRC/middleware/consoleMid1')
 const consoleMid2 = require('./SRC/middleware/consoleMid2')
@@ -24,6 +25,13 @@ app.use(notFound);
 //   console.log(`Server berjalan di http://localhost:${port}`)
 // );
 
-app.listen(port, function () {
-  return console.log(`Server berjalan di http://localhost:${port}`);
+app.listen(port, async() => {
+  try {
+    await sequelize.authenticate();
+    console.log(`Server berjalan di http://localhost:${port}`);
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  
 });
